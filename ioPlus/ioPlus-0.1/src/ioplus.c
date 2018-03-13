bcfe412b0e8a2affec5e7d34675bb640167130f3
dots-tb <dots_tb@gmx.com>
2018-03-13 00:18:07 -0700

    updated to new method

--- ioPlus/ioPlus-0.1/src/ioplus.c
+++ ioPlus/ioPlus-0.1/src/ioplus.c
@@ -15,22 +15,11 @@
 static int hooks_uid[HOOKS_NUMBER];
 static tai_hook_ref_t ref_hooks[HOOKS_NUMBER];
 
-typedef struct sceIoOpenOpt
-{
-  uint32_t unk_0;
-  uint32_t unk_4;
-} sceIoOpenOpt;
-
-static int _sceIoOpen_patched(const char *filename, int flag, SceIoMode mode, sceIoOpenOpt *opt) {
+static int ksceIoOpenForPid_patched(SceUID pid, const char *filename, int flag, SceIoMode mode) {
 	int ret, state;
 	ENTER_SYSCALL(state);
-	if ((ret = TAI_CONTINUE(int, ref_hooks[0], filename, flag, mode, opt)) < 0) { // try original function
-		char k_path[128];
-		ksceKernelStrncpyUserToKernel(&k_path, (uintptr_t)filename, sizeof(k_path));
-		printf("opened thanks to ioPlus: %s\n", k_path);
-		if ((ret = ksceIoOpen(k_path, flag, mode)) >= 0)
-			ret = ksceKernelCreateUserUid(ksceKernelGetProcessId(), ret);
-	}
+	if ((ret = TAI_CONTINUE(int, ref_hooks[0], pid, filename, flag, mode)) < 0)
+		ret = ksceIoOpen(filename, flag, mode);
 	EXIT_SYSCALL(state);
 	return ret;
 }
@@ -92,7 +81,7 @@ static int _sceIoRemove_patched(const char *filename, sceIoRemoveOpt* opt) {
 
 void _start() __attribute__ ((weak, alias ("module_start")));
 int module_start(SceSize argc, const void *args) {
-	hooks_uid[0] = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hooks[0], "SceIofilemgr", TAI_ANY_LIBRARY, 0xCC67B6FD, _sceIoOpen_patched);
+	hooks_uid[0] = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hooks[0], "SceIofilemgr", TAI_ANY_LIBRARY, 0xC3D34965, ksceIoOpenForPid_patched);
 	hooks_uid[1] = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hooks[1], "SceIofilemgr", TAI_ANY_LIBRARY, 0x8F1ACC32, _sceIoMkdir_patched);
 	hooks_uid[2] = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hooks[2], "SceIofilemgr", TAI_ANY_LIBRARY, 0xFFFB4D76, _sceIoRmdir_patched);
 	hooks_uid[3] = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hooks[3], "SceIofilemgr", TAI_ANY_LIBRARY, 0x78955C65, _sceIoRemove_patched);
