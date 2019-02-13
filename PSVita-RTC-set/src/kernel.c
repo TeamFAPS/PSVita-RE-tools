c219a62581bec37ad04d67c17372350490b0d7e7
CelesteBlue-dev <celesteblue.dev@gmail.com>
2019-02-13 14:29:52 +0100

    Add files via upload

--- PSVita-RTC-set/src/kernel.c
+++ PSVita-RTC-set/src/kernel.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 
 #include <psp2kern/kernel/modulemgr.h>
 #include <psp2kern/kernel/sysmem.h>
@@ -8,6 +7,7 @@
 
 #include <taihen.h>
 
+int module_get_export_func(SceUID pid, const char *modname, uint32_t libnid, uint32_t funcnid, uintptr_t *func);
 
 #define DUMP_PATH "ux0:data/"
 #define LOG_FILE DUMP_PATH "rtc_log.txt"
@@ -31,16 +31,20 @@ int module_start(SceSize argc, const void *args) {
 	// 0xDCB731F0F1C000 (1970)
 	// + 9676800000000 (diff 112 days Idk why)
 	// = 0xDCBFFEFF2BC000 = 62135596800000000
-	uint32_t startDate1970 = 1527811200; // seconds since 1970 - CHANGE WITH YOUR OWN VALUE
+	uint64_t startDate1970 = 1527811200; // seconds since 1970 - CHANGE WITH YOUR OWN VALUE
 	uint64_t startDate0 = (startDate1970 * 1000 * 1000) + 0xDCBFFEFF2BC000;
 
+	
 	// Setting CP RTC for DevKits
 	ret = module_get_export_func(KERNEL_PID, "SceSblPostSsMgr", TAI_ANY_LIBRARY, 0x3F9BDEDF, &stub);
 	LOG("ret = %08x\n", ret);
 	if (ret == 0){
-		void (*sceSblRtcMgrSetCpRtcForDriver) (int) = (void *) stub;
-		sceSblRtcMgrSetCpRtcForDriver(startDate1970); // 01-01-2000: 0x386D4381; 
+		int (*sceSblRtcMgrSetCpRtcForDriver) (int) = (void *) stub;
+		LOG("Updating CP RTC...\n");
+		ret = sceSblRtcMgrSetCpRtcForDriver(startDate1970); // 01-01-2000: 0x386D4381;
+		LOG("ret = %08x\n", ret);
 	}
+	
 	
 	// Setting Network RTC for TestKits
 	LOG("%llx\n", startDate0);
@@ -51,7 +55,7 @@ int module_start(SceSize argc, const void *args) {
 	timestamp[0] = ((unsigned int *)&startDate0)[0];
 	timestamp[1] = ((unsigned int *)&startDate0)[1];
 
-	LOG("Updating all Ticks.. ");
+	LOG("Updating all Ticks...\n");
 	ret = ksceRtcSetCurrentTick(timestamp);
 	LOG("ret = %08x\n", ret);
 	ret = ksceRtcSetCurrentNetworkTick(timestamp);
