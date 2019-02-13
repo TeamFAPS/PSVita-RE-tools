5015bad248f3091aef4522ffea6ac1037ef1116b
CelesteBlue-dev <celesteblue.dev@gmail.com>
2019-02-13 12:16:55 +0100

    Update kernel.c

--- PSVita-RTC-set/src/kernel.c
+++ PSVita-RTC-set/src/kernel.c
@@ -28,21 +28,21 @@ int module_start(SceSize argc, const void *args) {
 	uint32_t ret = -1;
 	uintptr_t stub;
 
-	/*
+	// 0xDCB731F0F1C000 (1970)
+	// + 9676800000000 (diff 112 days Idk why)
+	// = 0xDCBFFEFF2BC000 = 62135596800000000
+	uint32_t startDate1970 = 1527811200; // seconds since 1970 - CHANGE WITH YOUR OWN VALUE
+	uint64_t startDate0 = (startDate1970 * 1000 * 1000) + 0xDCBFFEFF2BC000;
+
+	// Setting CP RTC for DevKits
 	ret = module_get_export_func(KERNEL_PID, "SceSblPostSsMgr", TAI_ANY_LIBRARY, 0x3F9BDEDF, &stub);
 	LOG("ret = %08x\n", res);
 	if (ret == 0){
 		void (*sceSblRtcMgrSetCpRtcForDriver) (int) = (void *) stub;
-		sceSblRtcMgrSetCpRtcForDriver(0x00000001); // 01-01-2000: 0x386D4381; 
+		sceSblRtcMgrSetCpRtcForDriver(startDate1970); // 01-01-2000: 0x386D4381; 
 	}
-	*/
-
-	// 0xDCB731F0F1C000 (1970)
-	// + 9676800000000 (diff 112 days Idk why)
-	// = 0xDCBFFEFF2BC000 = 62135596800000000
-	uint64_t startDate1970 = 1527811200; // seconds since 1970
-	uint64_t startDate0 = (startDate1970 * 1000 * 1000) + 0xDCBFFEFF2BC000;
-
+	
+	// Setting Network RTC for TestKits
 	LOG("%llx\n", startDate0);
 	LOG("time0: %8x\n", ((unsigned int *)&startDate0)[0]);
 	LOG("time1: %8x\n", ((unsigned int *)&startDate0)[1]);
@@ -51,8 +51,7 @@ int module_start(SceSize argc, const void *args) {
 	timestamp[0] = ((unsigned int *)&startDate0)[0];
 	timestamp[1] = ((unsigned int *)&startDate0)[1];
 
-	LOG("Updating SecureTick.. ");
-
+	LOG("Updating all Ticks.. ");
 	ret = ksceRtcSetCurrentTick(timestamp);
 	LOG("ret = %08x\n", ret);
 	ret = ksceRtcSetCurrentNetworkTick(timestamp);
