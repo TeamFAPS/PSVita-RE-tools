6e7d08ee0137bc8afcacf1aaaf0996685d619937
CelesteBlue-dev <celesteblue.dev@gmail.com>
2019-03-03 09:44:07 +0100

    Add ARZL compressed data extraction from KBL

--- kbl_elf_extract/src/main.c
+++ kbl_elf_extract/src/main.c
@@ -28,7 +28,7 @@ typedef struct {
 int main(int argc, char **argv){
 	
 	if (argc < 2) {
-		printf ("\nusage: kbl_elf_extract file.elf/ \n");
+		printf("\nusage: kbl_elf_extract file.elf/ \n");
 		return -1;
 	}
 	
@@ -52,9 +52,9 @@ int main(int argc, char **argv){
 			SceModuleInfo *mod_info = (void *)input + offset + phdrs[0].p_offset + elf->e_entry;
 			printf("Mod_info  @%x\n", offset + phdrs[0].p_offset + elf->e_entry);
 			printf("EP  @%x\n", elf->e_entry);
-			if(!elf->e_entry) {
+			if (!elf->e_entry) {
 				printf("NO ENTRYPOINT DETECTED! Seeking .sceModuleInfo.rodata... \n");
-				if(elf->e_shnum > 0) {
+				if (elf->e_shnum > 0) {
 					Elf32_Shdr *shdr = (void *)input + offset+ elf->e_shoff;
 					for (int i = 0; i < elf->e_shnum; i++) {
 						char *sh_name = (void *)input + offset + shdr[elf->e_shstrndx].sh_offset + shdr[i].sh_name;
@@ -72,6 +72,15 @@ int main(int argc, char **argv){
 			
 			Elf32_Shdr *shdrs = (void *)input + offset + elf->e_shoff;
 			fwrite(input + offset, 1, shdrs[elf->e_shnum  - 1].sh_offset + shdrs[elf->e_shnum  - 1].sh_size, fp);
+			fclose(fp);
+		} else if (!strncmp((char *)(input+offset), "ARZL", 4)) {
+			printf("Found ARZL compressed data @%x\n", offset);
+			
+			char outname[32];
+			snprintf(outname, 32, "arzl_compressed_data_%X.bin", offset);
+			printf("Saving to: %s\n", outname);
+			fp = fopen(outname, "wb");
+			fwrite(input + offset, 1, sz - offset, fp);
 			fclose(fp);
 		}
 		offset++;
