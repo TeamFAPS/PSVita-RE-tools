19c3d0b8e7235b463471cd5f3173b4eecc2c334b
CelesteBlue-dev <celesteblue.dev@gmail.com>
2020-04-17 18:32:41 +0200

    Prettify code and reduces code length (73 lines)

--- ErrorResolver/src/ErrorResolver.c
+++ ErrorResolver/src/ErrorResolver.c
@@ -1,52 +1,35 @@
-//ErrorResolver.c - Resolves PSVita Error Codes!
+// ErrorResolver.c - PSVita error codes resolver by Silica and Princess Of Sleeping
 
 #include <stdio.h>
 #include <stdlib.h>
 
+
 unsigned int facility_group_num;
 unsigned int error_table_num;
 
-int read_error_table(const char *path, void *buf)
-{
-
-
+int read_error_table(const char *path, void *buf) {
 	FILE *f = fopen(path, "rb");
-	if (f)
-	{
-		if (fread(buf, 1, 8, f) == 8)
-		{
+	if (f) {
+		if (fread(buf, 1, 8, f) == 8) {
 			facility_group_num = *((short *)buf + 2);
-			if (facility_group_num <= 0x21)
-			{
+			if (facility_group_num <= 0x21) {
 				if (fread(*((int *)buf + 2), 1, 8 * facility_group_num, f) == 8 * facility_group_num
-					&& fread((char *)buf + 12, 1, 4, f) == 4)
-				{
+				&& fread((char *)buf + 12, 1, 4, f) == 4) {
 					error_table_num = *((short *)buf + 7);
-					if (error_table_num <= 0x4e21)
-					{
-						if (fread(*((int *)buf + 4), 1, 4 * error_table_num, f) == 4 * error_table_num)
-						{
+					if (error_table_num <= 0x4e21) {
+						if (fread(*((int *)buf + 4), 1, 4 * error_table_num, f) == 4 * error_table_num) {
 							fclose(f);
 							return 0;
 						}
-					}
-					else
-					{
+					} else
 						fprintf(stderr, "[SceError] error_table_num = %d\n", error_table_num);
-					}
 				}
-			}
-			else
-			{
+			} else
 				fprintf(stderr, "[SceError] facility_group_num = %d\n", facility_group_num);
-			}
 		}
 		fclose(f);
-	}
-	else
-	{
+	} else
 		printf("Failed to open error_table.bin\n");
-	}
 	*(int *)buf = 0;
 	*((int *)buf + 1) = 0;
 	*((int *)buf + 2) = 0;
@@ -56,8 +39,7 @@ int read_error_table(const char *path, void *buf)
 	return -1;
 }
 
-int error_code_to_string(int error_table, char *output_buffer, unsigned int Error_Code)
-{
+int error_code_to_string(int error_table, char *output_buffer, unsigned int Error_Code) {
 	unsigned int *error_list; // r5
 	int i; // r4
 	int ii; // r6
@@ -73,19 +55,15 @@ int error_code_to_string(int error_table, char *output_buffer, unsigned int Erro
 	if (!output_buffer)
 		return 0;
 	error_list = *(unsigned int **)(error_table + 0x10);
-	if (!*(short *)(error_table + 0xE))
-	{
+	if (!*(short *)(error_table + 0xE)) {
 	NOT_IN_DB:
 		snprintf(output_buffer, 0x10, "E-%08x", Error_Code);
 		return 0;
 	}
-	if (*error_list != Error_Code)
-	{
+	if (*error_list != Error_Code) {
 		i = 0;
-		while (++i < *(unsigned __int16 *)(error_table + 0xE))
-		{
-			if (error_list[1] == Error_Code)
-			{
+		while (++i < *(unsigned __int16 *)(error_table + 0xE)) {
+			if (error_list[1] == Error_Code) {
 				if (*(unsigned __int16 *)(error_table + 6) >= i)
 					goto CALC_FACILITY;
 				snprintf(output_buffer, 0x10, "*-%08x");
@@ -98,14 +76,11 @@ int error_code_to_string(int error_table, char *output_buffer, unsigned int Erro
 	i = 0;
 CALC_FACILITY:
 	Shifted = (Error_Code >> 16) & 0xFFF;
-	if ((unsigned int)(Shifted - 0x200) > 0xFF)
-	{
-		if (*(short *)(error_table + 4))
-		{
+	if ((unsigned int)(Shifted - 0x200) > 0xFF) {
+		if (*(short *)(error_table + 4)) {
 			error_table_32 = (int *)(*(int *)(error_table + 8) + 8);
 			ii = 0;
-			while (1)
-			{
+			while (1) {
 				error_table_16 = *((unsigned __int16 *)error_table_32 - 4);
 				++ii;
 				if (Shifted >= error_table_16 && Shifted < error_table_16 + *((unsigned __int16 *)error_table_32 - 3))
@@ -115,24 +90,18 @@ CALC_FACILITY:
 					goto Facility_Found;
 			}
 			facility = (char *)(error_table_32 - 1);
-		}
-		else
-		{
+		} else {
 		Facility_Found:
 			facility = (char *)error_table;
 		}
 	}
 	else
-	{
 		facility = "NS";
-	}
 	error_init = *(unsigned __int16 *)(error_table + 12) + i;
-	if (error_init)
-	{
+	if (error_init) {
 		Devider = error_init;
 		ii = 0;
-		do
-		{
+		do {
 			Remainder = Devider % 10;
 			Devider /= 10;
 			ii += Remainder;
@@ -140,22 +109,17 @@ CALC_FACILITY:
 		lastBit = ii % 10;
 	}
 	else
-	{
 		lastBit = 0;
-	}
 	snprintf(output_buffer, 16, "%s-%d-%1d", facility, error_init, lastBit);
 	return 0;
 }
 
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	char error_code[0x10];
 	int hex_error;
 
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		printf("ErrorResolver by Silica (with help from Princess Of Sleeping)\n");
 		printf("Reading error_table.bin\n");
 	}
@@ -166,13 +130,9 @@ int main(int argc, char *argv[])
 
 	int res = read_error_table("error_table.bin", error_table);
 	if (res != 0)
-	{
 		return res;
-	}
 
-	if (argc == 1)
-	{
-
+	if (argc == 1) {
 		printf("%d facility groups found in error_table.bin\n", facility_group_num);
 		printf("%d errors found in error_table.bin\n", error_table_num);
 		printf("Done!\n");
@@ -182,29 +142,23 @@ int main(int argc, char *argv[])
 		printf("\t-b Bruteforce hex from shortcode\n");
 		printf("No arguments = interactive\n\n\n");
 
-
 		printf("--Interactive Mode!\n");
 		printf("Resolve hex to shortcode = 1\n");
 		printf("Bruteforce shortcode to hex = 2\n");
+
 		printf("\nEnter choice: ");
 		int choice = 0;
 		scanf("%i", &choice);
-		if (choice == 1)
-		{
-			while (1)
-			{
+		if (choice == 1) {
+			while (1) {
 				printf("Enter Hex Code: ");
 				scanf("%x", &hex_error);
 
 				error_code_to_string(error_table, error_code, hex_error);
 				printf("Short Code: %s\n", error_code);
 			}
-
-		}
-		else if(choice == 2)
-		{
-			while (1)
-			{
+		} else if(choice == 2) {
+			while (1) {
 				int found = 0;
 				char target_error_code[0x10];
 
@@ -212,72 +166,45 @@ int main(int argc, char *argv[])
 				scanf("%16s",&target_error_code);
 
 				unsigned int* possible_options = (unsigned int*)error_table[4];
-				for (int i = 0; i < error_table_num; i++)
-				{
+				for (int i = 0; i < error_table_num; i++) {
 					hex_error = possible_options[i];
 					error_code_to_string(error_table, error_code, hex_error);
 
-					if (strcmp(error_code, target_error_code) == 0)
-					{
+					if (strcmp(error_code, target_error_code) == 0) {
 						printf("Hex Code: %x\n", hex_error);
 						found = 1;
 						break;
 					}
 				}
 				if (found == 0)
-				{
 					printf("%s Was not found in error_table.bin (perhaps its E- facility?)\n",target_error_code);
-				}
-				
 			}
-			
 		}
-
-		
-	}
-	else if (argc == 3)
-	{
+	} else if (argc == 3) {
 		char *mode = argv[1];
-		if (strcmp(mode, "-d") == 0)
-		{
+		if (strcmp(mode, "-d") == 0) {
 			sscanf(argv[2],"%x", &hex_error);
 			error_code_to_string(error_table, error_code, hex_error);
 			printf("%s\n", error_code);
-		}
-		else if (strcmp(mode, "-b") == 0)
-		{
+		} else if (strcmp(mode, "-b") == 0) {
 			char target_error_code[0x10];
 			strncpy(target_error_code, argv[2], 0x10);
 
 			unsigned int* possible_options = (unsigned int*)error_table[4];
-			for (int i = 0; i < error_table_num; i++)
-			{
+			for (int i = 0; i < error_table_num; i++) {
 				hex_error = possible_options[i];
 				error_code_to_string(error_table, error_code, hex_error);
 				
-				if (strcmp(error_code, target_error_code) == 0)
-				{
+				if (strcmp(error_code, target_error_code) == 0) {
 					printf("%x\n", hex_error);
 					return 0;
 				}
 			}
 			printf("%s Was not found in error_table.bin (perhaps its E- facility?)\n", target_error_code);
 			return -1;
-			
-			
-		}
-		else
-		{
+		} else
 			printf("Invalid arguments!\n");
-		}
-	}
-	else 
-	{
+	} else 
 		printf("Invalid arguments!\n");
-	}
-
-
-
     return 0;
 }
-
